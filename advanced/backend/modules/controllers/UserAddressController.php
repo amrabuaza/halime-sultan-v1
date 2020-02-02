da24dadb0b96e7e2763ef13d6c08d66698ff683c
amrabuaza <amr@insalat.com>
2020-02-02 11:36:46 +0200

    api (add user address controller)

---
+++ advanced/backend/modules/controllers/UserAddressController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace backend\modules\controllers;
+
+use yii\rest\ActiveController;
+
+class UserAddressController extends ActiveController
+{
+
+    public $modelClass = 'backend\models\UserAddress';
+
+}
